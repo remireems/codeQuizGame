64ded5a895bef554da868fe6daee5a82a15ddb98
Remi <75871744+remireems@users.noreply.github.com>
2021-08-16 18:38:46 -0700

    added questions in js

---
+++ script.js
@@ -0,0 +1,36 @@
+// add more code Qs later
+const questions = [
+  {
+    num: 1,
+    question: 'What does HTML stand for?',
+    answer: 'Hyper Text Markup Language',
+    options: [
+      'Hyper Text Preprocessor',
+      'Hyper Text Markup Language',
+      'Hyper Text Multiple Language',
+      'Hyper Tool Multi Language'
+    ]
+  },
+  {
+    num: 2,
+    question: 'What does CSS stand for?',
+    answer: 'Cascading Style Sheet',
+    options: [
+      'Cascading Style Sheet',
+      'Common Style Sheet',
+      'Color Style Sheet',
+      'Computer Style Sheet'
+    ]
+  },
+  {
+    num: 3,
+    question: 'What does FGO stand for?',
+    answer: 'Fate Grand Order',
+    options: [
+      'Fate Great Order',
+      'Fate Good Order',
+      'Fate God Order',
+      'Fate Grand Order'
+    ]
+  }
+]
